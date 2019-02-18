3b802776941fe7545ef54edd80bb5b497e3767f6
Matthew Herman <mattherman11@gmail.com>
2019-02-18 17:55:23 -0600

    Added sound

--- simon/simon.ino
+++ simon/simon.ino
@@ -1,7 +1,104 @@
+/*************************************************
+ * Public Constants
+ *************************************************/
+ 
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+
 typedef enum { GENERATE_SEQUENCE, WAIT_FOR_INPUT, GAME_OVER } state;
 typedef enum { RED, BLUE, YELLOW, GREEN, NONE } color;
 static const char *COLOR_STRING[] = {
-    "Red", "Blue", "Yellow", "Green", "None"
+  "Red", "Blue", "Yellow", "Green", "None"
+};
+static const int COLOR_TONE[] = {
+  NOTE_A5, NOTE_E5, NOTE_CS5, NOTE_E4
 };
 
 state currentState = GENERATE_SEQUENCE;
@@ -15,6 +112,9 @@ const byte RED_IN = 21;
 const byte BLUE_IN = 20;
 const byte YELLOW_IN = 19;
 const byte GREEN_IN = 18;
+
+const byte BUZZER = 7;
+const bool MUTE = true;
 
 const int MAX_SEQ_LEN = 5;
 
@@ -38,7 +138,7 @@ void setup() {
   pinMode(BLUE_IN, INPUT_PULLUP);
   pinMode(YELLOW_IN, INPUT_PULLUP);
   pinMode(GREEN_IN, INPUT_PULLUP);
-
+  
   attachInterrupt(digitalPinToInterrupt(RED_IN), redInput, CHANGE);
   attachInterrupt(digitalPinToInterrupt(BLUE_IN), blueInput, CHANGE);
   attachInterrupt(digitalPinToInterrupt(YELLOW_IN), yellowInput, CHANGE);
@@ -85,9 +185,9 @@ void outputSequence() {
     color next = sequence[i];
     Serial.print(COLOR_STRING[next]);
     Serial.print(" ");
-    digitalWrite(colorToPin(next), HIGH);
+    activateOutput(next);
     delay(250);
-    digitalWrite(colorToPin(next), LOW);
+    deactivateOutput(next);
     delay(500);
   }
   Serial.print("\n");
@@ -125,6 +225,16 @@ void waitForInput() {
   }
 }
 
+void activateOutput(color target) {
+  digitalWrite(colorToPin(target), HIGH);
+  if (!MUTE) tone(BUZZER, COLOR_TONE[target]);
+}
+
+void deactivateOutput(color target) {
+  digitalWrite(colorToPin(target), LOW);
+  noTone(BUZZER);
+}
+
 void resetInput() {
   lastInput = NONE;
   start = millis();
@@ -155,9 +265,9 @@ void handleInput(color selectedColor, int state) {
     Serial.print(COLOR_STRING[selectedColor]);
     Serial.println(" pressed..");
     lastInput = selectedColor;
-    digitalWrite(colorToPin(selectedColor), !state);
+    activateOutput(selectedColor);
   } else {
-    digitalWrite(colorToPin(selectedColor), !state);
+    deactivateOutput(selectedColor);
   }  
 }
 
@@ -167,18 +277,18 @@ void gameOver(bool win) {
   Serial.print("Win? ");
   Serial.println(win);
   
-  digitalWrite(RED_OUT, HIGH);
+  activateOutput(RED);
   delay(250);
-  digitalWrite(RED_OUT, LOW);
-  digitalWrite(BLUE_OUT, HIGH);
+  deactivateOutput(RED);
+  activateOutput(BLUE);
   delay(250);
-  digitalWrite(BLUE_OUT, LOW);
-  digitalWrite(YELLOW_OUT, HIGH);
+  deactivateOutput(BLUE);
+  activateOutput(YELLOW);
   delay(250);
-  digitalWrite(YELLOW_OUT, LOW);
-  digitalWrite(GREEN_OUT, HIGH);
+  deactivateOutput(YELLOW);
+  activateOutput(GREEN);
   delay(250);
-  digitalWrite(GREEN_OUT, LOW);
+  deactivateOutput(GREEN);
 }
 
 color pinToColor(int pin) {
